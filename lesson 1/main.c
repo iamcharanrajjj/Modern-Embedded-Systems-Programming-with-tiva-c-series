8380a34997aadb89dbc08c4c488a3a78237419ed
iamcharanrajjj <68603091+iamcharanrajjj@users.noreply.github.com>
2023-08-31 09:48:26 +0530

    Add files via upload

---
+++ lesson 1/main.c
@@ -0,0 +1,27 @@
+
+int main()
+{
+  unsigned int counter = 0;
+  ++counter;
+  ++counter;
+  ++counter;
+  ++counter;
+  ++counter;
+  ++counter;
+  ++counter;
+  ++counter;
+  ++counter;
+  ++counter;
+  ++counter;
+  ++counter;
+  ++counter;
+  ++counter;
+  ++counter;
+  ++counter;
+  ++counter;
+  ++counter;
+  ++counter;
+  ++counter;
+
+  return 0;
+}
