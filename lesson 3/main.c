1c86cbd9912c95cecebf4c98129cb6db2f261bd3
iamcharanrajjj <68603091+iamcharanrajjj@users.noreply.github.com>
2023-08-31 10:13:47 +0530

    Update main.c

--- lesson 3/main.c
+++ lesson 3/main.c
@@ -11,7 +11,7 @@ int main() {
   ptr = (int *)0x00202002u; 
 /*try this without type-casting u'll get an error, make sure that the breakpoint 
 is in the above line, then during debug use run to cursor position then use step into 
-so that, 0x00202002 address will be loaded in the register R0 */ 
+so that, 0x00202002 (ptr address) will be loaded manually in the register R0 */ 
 
   *ptr = 0xDEADBEEF; //only letter combinations from 10-15 i.e A-F
   return 0;
