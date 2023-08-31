04dd29f62f4826aa97e0c20604269289be2f1765
iamcharanrajjj <68603091+iamcharanrajjj@users.noreply.github.com>
2023-08-31 10:12:20 +0530

    Add files via upload

--- lesson 3/main.c
+++ lesson 3/main.c
@@ -8,7 +8,11 @@ int main() {
   ++(*ptr);
   }
   //playing with the pointers/ HACKS
-  ptr = (int *)0x00202002u; //try this without type-casting u'll get an error
+  ptr = (int *)0x00202002u; 
+/*try this without type-casting u'll get an error, make sure that the breakpoint 
+is in the above line, then during debug use run to cursor position then use step into 
+so that, 0x00202002 address will be loaded in the register R0 */ 
+
   *ptr = 0xDEADBEEF; //only letter combinations from 10-15 i.e A-F
   return 0;
 }
